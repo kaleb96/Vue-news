cdb31d33d92fd368c6b3272bdef8d6cbec439a4e
kaleb96 <yes505304@gmail.com>
2023-10-20 18:18:47 +0900

    Modified
    1. make ListItem component as Common

--- vue-news/src/components/ListItem.vue
+++ vue-news/src/components/ListItem.vue
@@ -1,19 +1,36 @@
 <template>
   <div>
     <ul class="news-list">
-      <li v-for="user in fetchedNews" class="post">
+      <li v-for="item in listItems" class="post">
         <div class="points">
-          {{ user.points }}
+          <!-- 포인트 영역 -->
+          {{ item.points || 0 }}
         </div>
         <div>
+          <!-- 타이틀 영역 -->
           <p class="news-title">
-            <a v-bind:href="user.url">
-              {{ user.title }}
-            </a>  
+            <!-- 분기처리 1 -->
+            <template v-if="item.domain">
+              <a v-bind:href="item.url">
+              {{ item.title }}
+            </a>
+            </template>
+          
+            <!-- 분기처리 2 -->
+            <template v-else>
+              <router-link v-bind:to="`item/${item.id}`">
+                {{ item.title }}
+              </router-link>
+            </template>  
           </p>
           <small class="link-text">
-          by 
-            <router-link v-bind:to="`/user/${user.user}`" class="link-text">{{  user.user }}</router-link> <!-- ES6-->
+            {{ item.time_ago }}by 
+            <router-link 
+              v-if="item.user"
+              v-bind:to="`/user/${item.user}`" class="link-text">{{ item.user }}</router-link>
+              <a :href="item.url" v-else>
+                {{ item.domain }}
+              </a>
           </small>
         </div>
       </li>
@@ -23,15 +40,65 @@
 
 <script>
 export default {
-created() {
-    this.$store.dispatch('FETCH_NEWS');
-    // fetchNewsList()
-    //   .then(response => this.users = response.data)
-    //   .catch(error => console.log(error))
+  created() {
+    
+    const name = this.$route.name;
+
+    if (name == 'news') {
+      this.$store.dispatch('FETCH_NEWS');
+    } else if (name == 'ask') {
+      this.$store.dispatch('FETCH_ASK');
+    } else if (name == 'jobs') {
+      this.$store.dispatch('FETCH_JOBS');
     }
+  },
+
+  computed: {
+    
+    listItems() {
+      const name = this.$route.name;
+
+      if(name === 'news') {
+        return this.$store.state.news;
+      } else if (name == 'ask') {
+        return this.$store.state.ask;
+      } else if (name =='jobs') {
+        return this.$store.state.jobs;
+      }
+    }
+  }
+
 }
 </script>
 
-<style>
+<style scoped>
+.news-list {
+  margin: 0;
+  padding: 0;
 
+}
+
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+
+.news-title {
+  margin:0;
+}
+
+.link-text {
+  color: #828282
+}
 </style>
--- vue-news/src/views/JobsView.vue
+++ vue-news/src/views/JobsView.vue
@@ -1,71 +1,17 @@
 <template>
   <div>
-    <ul class="job-list">
-      <li v-for="job in fetchedJobs" class="post">
-        <div class="comments_count">
-          {{ job.comments_count }}
-        </div>
-        <div>
-          <p class="job-title">
-            <a v-bind:href="job.url">{{ job.title }}</a>
-          </p>
-          <small class="link-text">
-            {{ job.time_ago }} by
-              <a :href="job.url"> {{ job.domain }}</a>
-          </small>
-        </div>
-      </li>
-    </ul>
-    <p >
-     
-     
-    </p>
+    <list-item></list-item>
   </div>
 </template>
 
 <script>
-import { mapGetters } from  'vuex'
-
+import ListItem from '../components/ListItem.vue'
 export default {
-  computed: {
-    ...mapGetters([
-        'fetchedJobs'
-    ])
-  },
-  created() {
-    this.$store.dispatch('FETCH_JOBS');
+  components: {
+    ListItem,
   }
 }
 </script>
 
 <style>
-.job-list {
-  margin: 0;
-  padding: 0;
-
-}
-
-.post {
-  list-style: none;
-  display: flex;
-  align-items: center;
-  border-bottom: 1px solid #eee;
-}
-
-.comments_count {
-  width: 80px;
-  height: 60px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: #42b883;
-}
-
-.job-title {
-  margin:0;
-}
-
-.text {
-  color: #828282
-}
 </style>
